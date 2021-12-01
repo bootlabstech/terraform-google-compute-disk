8f66bc743f58a48bac34bf976205697e99e29db5
Jayakumar6 <95211511+Jayakumar6@users.noreply.github.com>
2021-12-01 10:57:49 +0530

    first commit (#1)

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_disk" "disk" {
+    
+    name                            = var.name
+    type                            = var.type
+    zone                            = var.zone
+    image                           = var.image
+    physical_block_size_bytes       = var.physical_block_size_bytes
+    labels                          = var.labels
+    description                     = var.description
+    size                            = var.size
+    provisioned_iops                = var.provisioned_iops
+    snapshot                        = var.snapshot
+    project                         = var.project
+
+}
