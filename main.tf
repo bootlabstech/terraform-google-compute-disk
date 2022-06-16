4184c59c321ce0d2753eeb1ea5f08ca793da6ed7
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-06-16 11:57:12 +0530

    added terraform provider

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 resource "google_compute_disk" "disk" {
-    
-    name                            = var.name
-    type                            = var.type
-    zone                            = var.zone
-    image                           = var.image
-    physical_block_size_bytes       = var.physical_block_size_bytes
-    labels                          = var.labels
-    description                     = var.description
-    size                            = var.size
-    provisioned_iops                = var.provisioned_iops
-    snapshot                        = var.snapshot
-    project                         = var.project
 
+  name                      = var.name
+  type                      = var.type
+  zone                      = var.zone
+  image                     = var.image
+  physical_block_size_bytes = var.physical_block_size_bytes
+  labels                    = var.labels
+  description               = var.description
+  size                      = var.size
+  provisioned_iops          = var.provisioned_iops
+  snapshot                  = var.snapshot
+  project                   = var.project
 }
